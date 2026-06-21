b3c916f49163ec3da2181d42cb23694ce0e8ed17
AshmiC-JetLearner <ashmi.c32@gmail.com>
2026-06-21 12:47:07 -0400

    Update Growing_circle.py

--- Growing_circle.py
+++ Growing_circle.py
@@ -1,53 +1,65 @@
 import pygame
-screen=pygame.display.set_mode((600,600))
-screen.fill(('yellow'))
-blue=((0,0,255))
-red=((255,0,0))
-pygame.display.update()
+screen=pygame.display.set_mode([800,600])
+title=pygame.display.set_caption('My circle')
 
-class circle:
-    def __init__(self,color,radius,pos,width):
+
+blue=(0,0,255)
+red=(255,0,0)
+green=(0,255,0)
+screen.fill(('white'))
+class my_circle:
+    def __init__(self,color,pos,radius,width=0):
         self.circle_color=color
-        self.circle_radius=radius
         self.circle_pos=pos
+        self.circle_radius=radius
         self.circle_width=width
         self.circle_surface=screen
 
     def draw(self):
-        self.draw_circle=pygame.draw.circle(self.circle_surface,
+        pygame.draw.circle(self.circle_surface,
                                             self.circle_color,
                                             self.circle_pos,
                                             self.circle_radius,
-                                            self.circle_width
+                                            self.circle_width,
                                             )
-    
+        
     def grow(self,r):
         self.circle_radius=self.circle_radius+r
-        self.draw_circle=pygame.draw.circle(self.circle_surface,
+        pygame.draw.circle(self.circle_surface,
                                             self.circle_color,
                                             self.circle_pos,
                                             self.circle_radius,
                                             self.circle_width
                                             )
-
-#creating objects
-circle1=circle('blue',30,(300,300),20)
-circle2=circle('red',70,(100,450),0)
+        
+#creating objects for the class  
+circle1=my_circle(blue,(400,400),20)
+circle2=my_circle(red,(100,450),15)
+circle3=my_circle(green,(200,100),30)
 
 running=True
+
 while running:
     for event in pygame.event.get():
-        if event.type==pygame.QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
 
-        if event.type==pygame.MOUSEBUTTONDOWN:
-            screen.fill('yellow')
-            circle1.draw()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             circle2.draw()
+            circle1.draw()
+            circle3.draw()
+            pygame.display.update()
+        
+        elif event.type == pygame.MOUSEBUTTONUP:
+            circle1.grow(4)
+            circle2.grow(3)
+            circle3.grow(2)
+            pygame.display.update()
+        
+        elif event.type == pygame.MOUSEMOTION:
+            pos=pygame.mouse.get_pos()
+            r_circle=my_circle(red,pos,5)
+            r_circle.draw()
             pygame.display.update()
 
-
-        elif event.type==pygame.MOUSEBUTTONUP:
-            circle1.grow(5)
-            circle2.grow(7)
-            pygame.display.update()
+        
